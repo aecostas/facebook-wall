7bb12b446fb4f6cef4fe909f654c92307bf0aed0
Andrés Estévez <aecostas@gmail.com>
2018-03-14 17:12:33 +0100

    Join all posts

--- main.go
+++ main.go
@@ -23,47 +23,49 @@ type Container struct {
 	Paging Navigation
 }
 
-func request(url string) (*Container, error){
+func request(url string) (*Container, error) {
+	var m Container
+
 	resp, err := http.Get(url)
 
 	if err != nil || (resp.Status != "200 OK") {
 		fmt.Println("Error getting URL")
 		fmt.Println("HTTP Status: ", resp.Status)
-
-		return  nil,err
-	}
-
-	fmt.Println("HTTP Status: ", resp.Status)
-	fmt.Println("ContentLength: ", resp.ContentLength)
-	fmt.Println("Close: ", resp.Close)
-	fmt.Println("TransferEncoding: ", resp.TransferEncoding)
-
-	if resp.ContentLength < 0 {
-		fmt.Println("No Data returned")
+		return  nil, err
 	}
 
 	data, err := ioutil.ReadAll(resp.Body)
 
-	if err == nil {
+	if err != nil {
 		fmt.Println(err)
+		return nil, err
 	}
 
-	var m Container
 	err = json.Unmarshal(data, &m)
 
 	if err != nil {
 		fmt.Println("json.Unmarshal -> %v", err)
+		return nil, err
 	}
-
 	return &m, nil
 }
 
 func main() {
 	var container *Container
-	fmt.Println("starting...")
-	var eaccessToken string = "EAACEdEose0cBAEVKkfn3GjCBBWVjBBvwuDAoN4uwWeuxqyE1bJJd9FSDkgpGJ4dDzpJVSg4kBkNH4XG64TpsetX6r5igQoiTzEiDCsAHztLX5xpSwd4TZAxffRlsKle2vvGEsMwOuRGVZBRNdThB2jQTQjKMwnf5ZAsN9aOMmmrnkFB1mldZCMQkQi4RqDWv5u11vGD4gAZDZD"
-	var url string = "https://graph.facebook.com/v2.12/10153862771857156/posts?limit=25&until=1517841695&__paging_token=enc_AdBLO4Ynl6ZA7DY4O68GKcR2SGufSw4fJOK3AeWwfZAKZCOxKDhNmTnBshouGFiAZBcj8Kyelu24hvr8y7o5aUiLIIKh&access_token=" + eaccessToken
-	container, _ = request(url)
+	var posts []Posts
 
-	fmt.Println(container.Paging.Next)
+	var eaccessToken string = "EAACEdEose0cBAJamrYHMywKXwI3uxc8GJeZC6beW12lazKcSMF1ktpLjZAqeFT4lO3rRZBSuylTFC62vrBZAG5j4amH9QpcPv3OHACk99goSaJ4ls1ZAInkPyh0pHLYiXxCV3eSAEIZCNK6SGRIZAvWrcK0lt3Tywt1ZB5BMWzCKEdmwTDlcamNogAKbUYcdVTGSb540B9m3kgZDZD"
+	var url string = "https://graph.facebook.com/v2.12/10153862771857156/posts?limit=25&until=1517841695&__paging_token=enc_AdBLO4Ynl6ZA7DY4O68GKcR2SGufSw4fJOK3AeWwfZAKZCOxKDhNmTnBshouGFiAZBcj8Kyelu24hvr8y7o5aUiLIIKh&access_token=" + eaccessToken
+
+	for {
+		container, _ = request(url)
+		posts = append(posts, container.Data...)
+		if container.Paging.Next == "" {
+			break;
+		}
+
+		url = container.Paging.Next
+	}
+
+	fmt.Println(len(posts))
 }
