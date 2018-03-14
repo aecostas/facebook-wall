9b67c634b746b7588624b85479484080b68eb876
Andrés Estévez <aecostas@gmail.com>
2018-03-14 13:43:22 +0100

    Return error on request

--- main.go
+++ main.go
@@ -23,14 +23,14 @@ type Container struct {
 	Paging Navigation
 }
 
-func request(url string) {
+func request(url string) (*Container, error){
 	resp, err := http.Get(url)
 
 	if err != nil || (resp.Status != "200 OK") {
 		fmt.Println("Error getting URL")
 		fmt.Println("HTTP Status: ", resp.Status)
 
-		return
+		return  nil,err
 	}
 
 	fmt.Println("HTTP Status: ", resp.Status)
@@ -53,20 +53,17 @@ func request(url string) {
 
 	if err != nil {
 		fmt.Println("json.Unmarshal -> %v", err)
-	} else {
-		fmt.Println("=== Received data:")
-		fmt.Println(m.Paging.Next)
-
-//		for i := 0; i<len(m.Data); i++ {
-//			fmt.Println("===========================")
-//			fmt.Println(m.Data[i])
-//		}
 	}
+
+	return &m, nil
 }
 
 func main() {
+	var container *Container
 	fmt.Println("starting...")
+	var eaccessToken string = "EAACEdEose0cBAEVKkfn3GjCBBWVjBBvwuDAoN4uwWeuxqyE1bJJd9FSDkgpGJ4dDzpJVSg4kBkNH4XG64TpsetX6r5igQoiTzEiDCsAHztLX5xpSwd4TZAxffRlsKle2vvGEsMwOuRGVZBRNdThB2jQTQjKMwnf5ZAsN9aOMmmrnkFB1mldZCMQkQi4RqDWv5u11vGD4gAZDZD"
 	var url string = "https://graph.facebook.com/v2.12/10153862771857156/posts?limit=25&until=1517841695&__paging_token=enc_AdBLO4Ynl6ZA7DY4O68GKcR2SGufSw4fJOK3AeWwfZAKZCOxKDhNmTnBshouGFiAZBcj8Kyelu24hvr8y7o5aUiLIIKh&access_token=" + eaccessToken
-	request(url)
+	container, _ = request(url)
 
+	fmt.Println(container.Paging.Next)
 }
